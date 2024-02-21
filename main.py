dd32ef0a18c6d53af3d013461a6de117a860fbab
Cameron <cameron-04@hotmail.com>
2024-02-21 17:37:42 -0600

    add ability to load and deploy services

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from kubernetes import config
 
 from cluster import clear_cluster
 from deployment import DeploymentPreset, deploy_deployment
+from service import ServicePreset, deploy_service
 
 if __name__ == '__main__':
     config.load_kube_config()
-    clear_cluster()
-    deploy_deployment("default", DeploymentPreset.FRONT)
+    # clear_cluster()
+    # deploy_deployment("default", DeploymentPreset.FRONT)
+    deploy_service("default", ServicePreset.FRONT)
